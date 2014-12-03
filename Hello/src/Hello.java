8679d58b0732824ee72b963735f642ba7dbc875d
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2014-12-03 18:13:01 +0800

    my ambot commit

---
+++ Hello/src/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)Hello.java
+ *
+ * Hello application
+ *
+ * @author 
+ * @version 1.00 2014/11/19
+ */
+ 
+public class Hello {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
